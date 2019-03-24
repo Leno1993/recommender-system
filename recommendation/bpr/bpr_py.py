955d020bcd7bdbe7aeb02935e4f4352e607ae67d
wangshengguang <1085092799@qq.com>
2019-03-24 11:29:46 +0800

    ADD: bpr_py some fix

--- recommendation/bpr/bpr_py.py
+++ recommendation/bpr/bpr_py.py
@@ -23,8 +23,8 @@ class BPR(object):
         """模型加载"""
         if not saved_model_path:
             saved_model_path = self.get_min_loss_model_path()
-        U, V = np.load(saved_model_path)
-        return U, V
+        U, V, biasV = np.load(saved_model_path)
+        return U, V, biasV
 
     def get_min_loss_model_path(self, checkpoint_dir=checkpoint_dir):
         # 获取loss最小的模型的路径
@@ -35,28 +35,28 @@ class BPR(object):
 
     def train_step(self, batch_samples, update_parm=True):
         """一次训练迭代过程"""
-        u = batch_samples[:, 0] - 1  # 编号-1成索引
+        u = batch_samples[:, 0] - 1  # index = user_id-1
         i = batch_samples[:, 1] - 1
         j = batch_samples[:, 2] - 1
-        r_ui = np.dot(self.U[u], self.V[i].T) + self.biasV[i]
-        r_uj = np.dot(self.U[u], self.V[j].T) + self.biasV[j]
+        r_ui = np.dot(self.U[u], self.V[i].T) + self.biasV[i]  # predict prob
+        r_uj = np.dot(self.U[u], self.V[j].T) + self.biasV[j]  # predict prob
         r_uij = r_ui - r_uj
-        loss_func = -1.0 / (1 + np.exp(r_uij))
+        loss_func = 1.0 / (1 + np.exp(r_uij))
         loss_func = np.mean(loss_func)
         # update U and V
         if update_parm:
-            self.U[u] += -self.lr * (loss_func * (self.V[i] - self.V[j]) + self.reg * self.U[u])
-            self.V[i] += -self.lr * (loss_func * self.U[u] + self.reg * self.V[i])
-            self.V[j] += -self.lr * (loss_func * (-self.U[u]) + self.reg * self.V[j])
+            self.U[u] += self.learning_rate * (loss_func * (self.V[i] - self.V[j]) - self.reg * self.U[u])
+            self.V[i] += self.learning_rate * (loss_func * self.U[u] - self.reg * self.V[i])
+            self.V[j] += self.learning_rate * (-loss_func * self.U[u] - self.reg * self.V[j])
             # update biasV
-            self.biasV[i] += -self.lr * (loss_func + self.reg * self.biasV[i])
-            self.biasV[j] += -self.lr * (-loss_func + self.reg * self.biasV[j])
+            self.biasV[i] += self.learning_rate * (loss_func - self.reg * self.biasV[i])
+            self.biasV[j] += self.learning_rate * (-loss_func - self.reg * self.biasV[j])
         return loss_func
 
     def train(self):
         """训练"""
         self.latent_factors = 20
-        self.lr = 0.01
+        self.learning_rate = 0.01
         self.reg = 0.01  # 正则化参数
         max_u_id = max(self.data_helper.user_ratings.keys())
         max_i_id = max(self.data_helper.item_ids)
@@ -64,29 +64,59 @@ class BPR(object):
         self.V = np.random.rand(max_i_id, self.latent_factors) * 0.01
         self.biasV = np.random.rand(max_i_id) * 0.01
         step = 0
-        for batch_samples in self.data_helper.generate_samples(epoch_num=10, batch_size=3):
+        for batch_samples in self.data_helper.generate_samples(epoch_num=10, batch_size=8):
             train_loss = self.train_step(batch_samples)
             step += 1
             if step % 10000 == 0:
+                # validate, 使用验证集数据选择模型
                 loss = []
                 for batch_samples in self.data_helper.generate_samples(batch_size=1000, data_type="validate"):
                     _loss = self.train_step(batch_samples, update_parm=False)
                     loss.append(_loss)
                 val_loss = np.mean(loss)
-                print("step:{}, loss: {}, val_loss:{}".format(step, train_loss, val_loss))
+                print("step:{}, loss: {:.4f}, val_loss:{:.4f}".format(step, train_loss, val_loss))
                 model_path = os.path.join(checkpoint_dir, "{}_{:.4f}.npy".format(step, val_loss))
-                np.save(model_path, [self.U, self.V])
+                np.save(model_path, [self.U, self.V, self.biasV])
+        print("\n")
 
-    def predict(self, user_id):
+    def test(self):
+        def samples_auc(batch_samples):
+            u = batch_samples[:, 0] - 1  # index = user_id-1
+            i = batch_samples[:, 1] - 1
+            j = batch_samples[:, 2] - 1
+            r_ui = np.dot(self.U[u], self.V[i].T) + self.biasV[i]  # predict
+            r_uj = np.dot(self.U[u], self.V[j].T) + self.biasV[j]  # predict
+            r_uij = r_ui - r_uj
+            return r_uij
+
+        auc = []
+        loss = []
+        for batch_samples in self.data_helper.generate_samples(batch_size=1000, data_type="test"):
+            r_uij = samples_auc(batch_samples)
+            _loss = self.train_step(batch_samples, update_parm=False)
+            auc.append(r_uij)
+            loss.append(_loss)
+        auc = np.array(auc)
+        test_auc = np.sum(auc > 0) / np.prod(auc.shape)
+        test_loss = np.mean(loss)
+
+        print("test_loss:{:.4f}, test_auc:{:.4f}\n".format(test_loss, test_auc))
+
+    def predict(self, user_id, topk=3):
         """预测推荐"""
-        U, V = self.load_model()
-        _predict = np.dot(U[user_id], V.T)
-        _predict = largest_indices(_predict, 3)
-        return _predict
+        U, V, biasV = self.load_model()
+        _predict = np.dot(U[user_id - 1], V.T) + biasV  # index = user_id-1
+        top_n_index = largest_indices(_predict, topk)  # top 3
+        # score = _predict[top_n_index]
+        # pred = list(zip(top_n_index + 1, score))  # user_id = index+1
+        pred = top_n_index + 1  # user_id = index+1
+        return pred
 
 
 if __name__ == '__main__':
     bpr = BPR()
     bpr.train()
-    pred = bpr.predict(0)
-    print("predict:{}".format(pred))
+    bpr.test()
+    user_id = 10
+    pred = bpr.predict(user_id)
+    print("user_id: {}, predict:{}\n".format(user_id, pred))
--- recommendation/bpr/bpr_tf.py
+++ recommendation/bpr/bpr_tf.py
@@ -45,8 +45,8 @@ with tf.Graph().as_default(), tf.Session() as session:
     user_count = max(data_helper.user_ratings.keys())
     item_count = max(data_helper.item_ids)
     hidden_dim = 20
-    epoch_num = 1
-    batch_size = 8
+    epoch_num = 10
+    batch_size = 16
 
     u, i, j, mf_auc, bprloss, train_op = bpr_mf(user_count, item_count, hidden_dim)
     saver = tf.train.Saver(tf.global_variables(), max_to_keep=5)
@@ -63,7 +63,7 @@ with tf.Graph().as_default(), tf.Session() as session:
                                           feed_dict={u: uij[:, 0], i: uij[:, 1], j: uij[:, 2]})
             step += 1
             if step % 1000 == 0:
-                print("step:{}, loss: {}".format(step, loss))
+                print("step:{}, loss: {:.4f}".format(step, loss))
                 path = saver.save(session, checkpoint_prefix, global_step=step)
                 print("Saved model checkpoint to {}\n".format(path))
 
@@ -75,7 +75,7 @@ with tf.Graph().as_default(), tf.Session() as session:
             _auc, _test_bprloss = session.run([mf_auc, bprloss],
                                               feed_dict={u: t_uij[:, 0], i: t_uij[:, 1], j: t_uij[:, 2]})
             auc.append(_auc), loss.append(_test_bprloss)
-        print("test loss:{}, test auc:{} ".format(np.mean(auc), np.mean(loss)))
+        print("test loss:{:.4f}, test auc:{:.4f} ".format(np.mean(auc), np.mean(loss)))
         print("")
         return loss, auc
 
@@ -100,11 +100,12 @@ def load_model():
 def predict(user_id):
     """预测推荐"""
     U, V = load_model()
-    _predict = np.dot(U[user_id], V.T)
-    _predict = largest_indices(_predict, 3)
+    _predict = np.dot(U[user_id - 1], V.T)  # index = user_id-1
+    _predict = largest_indices(_predict, 3) + 1  # top 3 ; user_id = index+1
     return _predict
 
 
-pred = predict(0)
+user_id = 1
+pred = predict(user_id)
 
-print("predict: {}".format(pred))
+print("user_id:{}, predict: {}".format(user_id, pred))
